f3affd0e1d2bef4668e8a1d37fd305ca0e0c50d8
Marshall <marshallstutzman@gmail.com>
2025-01-04 22:56:43 -0500

    works even better, needs cleaned up

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::{io, thread, time};
-use std::io::{stdout, Write};
+use std::io::{stdout, Read, Write};
 
 mod parse;
 use parse::get_time;
@@ -15,6 +15,13 @@ use parse::get_time;
 
 const DEFAULT: &str = "25:00";
 const INPUT_MESSAGE: &str = "Press enter for 25 minutes or enter a time in HH:MM:SS format. Press [Q] to exit.";
+const TIMES_UP: &str = "
+████████ ██ ███    ███ ███████ ███████     ██    ██ ██████  ██ 
+   ██    ██ ████  ████ ██      ██          ██    ██ ██   ██ ██ 
+   ██    ██ ██ ████ ██ █████   ███████     ██    ██ ██████  ██ 
+   ██    ██ ██  ██  ██ ██           ██     ██    ██ ██         
+   ██    ██ ██      ██ ███████ ███████      ██████  ██      ██
+";
 
 fn main() {
     
@@ -48,16 +55,14 @@ fn main() {
         }
     }
 
-
-    // print!("\x1B[2J\x1B[1;1H"); // clears console, takes cursor to first row first column
+    print!("\x1B[2J\x1B[1;1H"); // clears console, takes cursor to first row first column
 
     loop {
         print!("{hr:0>2}:{min:0>2}:{sec:0>2}\r");
         stdout.flush().expect("fluuuuuush"); // TODO
-        thread::sleep(time::Duration::from_secs(1));
+
         if hr == 0 && min == 0 && sec == 0 {
-            // times up
-            todo!();
+            break;
         }
 
         if sec == 0 {
@@ -72,6 +77,21 @@ fn main() {
         }
 
         sec -= 1;
+
+        thread::sleep(time::Duration::from_secs(1));
+    }
+
+    print!("\x1B[2J\x1B[1;1H");
+    println!("{}\n\n", TIMES_UP);
+    println!("Hit [Enter]");
+    loop {
+        let mut input: Vec<u8> = Vec::new();
+
+        if let Err(e) = io::stdin().read(&mut input) {
+           continue;
+        } else {
+           return;
+        }
     }
 
 }
